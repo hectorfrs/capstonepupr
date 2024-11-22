d29d22a8bd278d7a69d10f6e084dc946077f9753
hectorfrs <67123958+hectorfrs@users.noreply.github.com>
2024-11-22 19:53:00 -0400

    Se añade los distintos tipos de plasticos en una categoria.

--- aws/src/functions/waste-type/waste_type.py
+++ aws/src/functions/waste-type/waste_type.py
@@ -36,6 +36,7 @@ def lambda_handler(event, context):
             event[0]["organic_waste"] = 0
             event[0]["solid_waste"] = 0
             event[0]["hazardous_waste"] = 0
+            event[0]["plastic_types"] = {}
             event[0]["other_waste"] = 0
             return event
         # Get the custom labels
@@ -46,25 +47,28 @@ def lambda_handler(event, context):
         hazardous_waste = 0
         other_waste = 0
         sorted_waste_items = []
+        # Dictionaries for waste categories
         organic_waste_dict = [
-            "orange",
-            "bread",
-            "banana",
-            "orange peel",
-            "apple",
-            "onion",
-            "vegatable",
-            "potato",
+            "orange", "bread", "banana", "orange peel", "apple", "onion", 
+            "vegetable", "potato"
         ]
         solid_waste_dict = [
-            "cardboard",
-            "plastic",
-            "paper",
-            "bottle",
-            "polethene",
-            "paper ball",
+            "cardboard", "plastic", "paper", "bottle", "polythene", "paper ball"
         ]
         hazardous_waste_dict = ["batteries"]
+         # Plastic subcategories
+        plastic_types_dict = {
+            "PET": ["bottle", "polyethylene terephthalate", "soft drink bottle"],
+            "HDPE": ["high-density polyethylene", "plastic bag", "milk jug"],
+            "PVC": ["polyvinyl chloride", "pipe", "vinyl"],
+            "LDPE": ["low-density polyethylene", "plastic wrap", "grocery bag"],
+            "PP": ["polypropylene", "yogurt container", "straw"],
+            "PS": ["polystyrene", "styrofoam", "disposable cup"],
+            "Other": ["bioplastics", "unknown plastic"]
+        }
+
+        plastic_types = {key: 0 for key in plastic_types_dict.keys()}
+
         for label in labels:
             if label["Name"] in organic_waste_dict:
                 organic_waste += 1
@@ -74,13 +78,21 @@ def lambda_handler(event, context):
                 hazardous_waste += 1
             else:
                 other_waste += 1
+
+            # Check for plastic subcategories
+            for plastic_type, keywords in plastic_types_dict.items():
+                if any(keyword in label_name for keyword in keywords):
+                    plastic_types[plastic_type] += 1
+
             if label["Name"] not in sorted_waste_items:
                 sorted_waste_items.append(label["Name"])
+
         event[0]["sorted_waste"] = sorted_waste_items
         event[0]["organic_waste"] = organic_waste
         event[0]["solid_waste"] = solid_waste
         event[0]["hazardous_waste"] = hazardous_waste
         event[0]["other_waste"] = other_waste
+        event[0]["plastic_types"] = plastic_types
         event[0]["event_date"] = get_event_date(event)
         print(event)
     else:
@@ -90,5 +102,6 @@ def lambda_handler(event, context):
         event[0]["solid_waste"] = 0
         event[0]["hazardous_waste"] = 0
         event[0]["other_waste"] = 0
+        event[0]["plastic_types"] = {}
         event[0]["event_date"] = get_event_date(event)
     return event
