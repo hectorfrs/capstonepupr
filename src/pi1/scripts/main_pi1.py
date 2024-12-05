1157e4e355aa81d833e903a0a3e534dcebcc4f92
hectorfrs <67123958+hectorfrs@users.noreply.github.com>
2024-12-05 11:24:11 -0400

    actualizacion

--- src/pi1/scripts/main_pi1.py
+++ src/pi1/scripts/main_pi1.py
@@ -28,6 +28,7 @@ from logging.handlers import RotatingFileHandler
 # Agregar la ruta del proyecto al PYTHONPATH
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 class StreamToLogger:
     """
