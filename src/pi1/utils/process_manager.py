972905377c13ab898471311061f5efea325645c8
hectorfrs <67123958+hectorfrs@users.noreply.github.com>
2024-12-13 16:38:16 -0400

    update

--- src/pi1/utils/process_manager.py
+++ src/pi1/utils/process_manager.py
@@ -1,8 +1,8 @@
 import logging
 import time
 
-from lib.AS7265x_HighLevel import AS7265xSensorHighLevel
-from lib.TCA9548A_HighLevel import TCA9548AMUXHighLevel
+from lib.AS7265x_HighLevel import AS7265x
+from lib.TCA9548A_HighLevel import TCA9548A
 from utils.identify_plastic_type import identify_plastic_type
 
 
