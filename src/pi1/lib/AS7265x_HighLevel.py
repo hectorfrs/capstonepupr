a64ce167a848d19e070b07121fba2d1a8537ebfe
hectorfrs <67123958+hectorfrs@users.noreply.github.com>
2024-12-13 08:45:23 -0400

    update

--- src/pi1/lib/AS7265x_HighLevel.py
+++ src/pi1/lib/AS7265x_HighLevel.py
@@ -46,7 +46,7 @@ class AS7265xSensorHighLevel:
         spectrum = self.sensor.read_calibrated_spectrum()
         formatted_spectrum = json.dumps(spectrum, indent=4)
         if self.config['system']['enable_sensor_diagnostics']:
-            self.diagnostic_check(spectrum, self.config)
+            self.diagnostic_check(spectrum)
         logging.info(f"Espectro calibrado leído: \n{formatted_spectrum}")
         return spectrum
 
@@ -102,7 +102,7 @@ class AS7265xSensorHighLevel:
         """
         return self.sensor._read_status()
 
-    def diagnostic_check(spectrum, self):
+    def _diagnostic_check(self, spectrum):
         """
         Verifica la calidad de los datos espectrales y genera alertas si son inconsistentes.
         """
@@ -111,9 +111,10 @@ class AS7265xSensorHighLevel:
                 logging.warning("Diagnóstico: Todos los valores del espectro son 0.")
             if max(spectrum.values()) > 3000:
                 logging.warning("Diagnóstico: Valor excesivo detectado en el espectro.")
-            if config['system']['enable_detailed_logging']:
+            if self.config['system']['enable_detailed_logging']:
                 logging.debug(f"Datos espectrales detallados: {spectrum}")
             logging.info("Diagnóstico completado exitosamente.")
         except Exception as e:
             logging.error(f"Error en diagnóstico del espectro: {e}")
+
 
