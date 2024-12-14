6a73b4b4c2bd24d4632b5001727459b93cb909b0
hectorfrs <67123958+hectorfrs@users.noreply.github.com>
2024-12-14 15:56:55 -0400

    update

--- src/pi1/classes/AS7265x_Controller.py
+++ src/pi1/classes/AS7265x_Controller.py
@@ -43,7 +43,9 @@ class SENSOR_AS7265x:
         self.address = address
         logging.info(f"[CONTROLLER] [SENSOR] AS7265x inicializado en dirección {hex(self.address)} en el bus I2C {i2c_bus}.")
         self.verify_connection()
+        
         self.configure(integration_time=100, gain=1, mode=0) 
+        logging.debug("[CONTROLLER] [SENSOR] Configuración inicial completada.")
 
     def check_sensor_status(self):
         return self.verify_ready_state()
