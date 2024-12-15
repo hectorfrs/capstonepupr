46778088d749c3bfc4762399d6bf57220b73b42b
hectorfrs <67123958+hectorfrs@users.noreply.github.com>
2024-12-15 16:18:51 -0400

    update

--- src/pi1/lib/TCA9548A_HighLevel.py
+++ src/pi1/lib/TCA9548A_HighLevel.py
@@ -22,7 +22,7 @@ class TCA9548A_Manager:
         """
         self.i2c_bus = i2c_bus
         self.address = address
-        self.mux = qwiic_tca9548a.QwiicTCA9548A(address=address, i2c_bus=i2c_bus)
+        self.mux = MUX_TCA9548A(address=address, i2c_bus=i2c_bus)
         logging.info(f"[MANAGER] [MUX] TCA9548A inicializado en la dirección {hex(address)}.")
 
     def enable_channel(self, channel):
