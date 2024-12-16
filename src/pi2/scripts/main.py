073846c154a6c9e1b7255229aa676d6433e5d281
hectorfrs <67123958+hectorfrs@users.noreply.github.com>
2024-12-16 12:43:42 -0400

    update

--- src/pi2/scripts/main.py
+++ src/pi2/scripts/main.py
@@ -6,7 +6,7 @@
 import json
 import logging
 from utils.mqtt_client import create_mqtt_client, subscribe_to_topic, publish_message
-from lib.relay_controller import RelayController
+#from lib.relay_controller import RelayController
 from utils.network_manager import NetworkManager
 from utils.real_time_config import RealTimeConfigManager
 from utils.config_manager import ConfigManager
@@ -24,8 +24,8 @@ def main():
         network_manager.start_monitoring()
 
         # Configuración del controlador de relés
-        relay_addresses = [0x18, 0x19]  # Direcciones I2C de los relés
-        relay_controller = RelayController(relay_addresses)
+        #relay_addresses = [0x18, 0x19]  # Direcciones I2C de los relés
+        #relay_controller = RelayController(relay_addresses)
 
         # Variables de configuración MQTT
         broker_addresses = config['mqtt']['broker_addresses']
@@ -35,15 +35,15 @@ def main():
         topic_status = config['mqtt']['topics']['status']
 
         # Función para manejar el control de los relés según el tipo de material
-        def handle_relay_control(material_type, duration):
-            if material_type == 'PET':
-                relay_controller.activate_relay(0, duration)  # Activar relé 1
-                logging.info(f"[MAIN] [RELAY] Activando Relay 1 (PET) por {duration} segundos")
-            elif material_type == 'HDPE':
-                relay_controller.activate_relay(1, duration)  # Activar relé 2
-                logging.info(f"[MAIN] [RELAY] Activando Relay 2 (HDPE) por {duration} segundos")
-            else:
-                logging.info(f"[MAIN] [RELAY] Material desconocido: {material_type}")
+        # def handle_relay_control(material_type, duration):
+        #     if material_type == 'PET':
+        #         relay_controller.activate_relay(0, duration)  # Activar relé 1
+        #         logging.info(f"[MAIN] [RELAY] Activando Relay 1 (PET) por {duration} segundos")
+        #     elif material_type == 'HDPE':
+        #         relay_controller.activate_relay(1, duration)  # Activar relé 2
+        #         logging.info(f"[MAIN] [RELAY] Activando Relay 2 (HDPE) por {duration} segundos")
+        #     else:
+        #         logging.info(f"[MAIN] [RELAY] Material desconocido: {material_type}")
 
         # Callback para manejar mensajes MQTT
         def on_message(client, userdata, msg):
