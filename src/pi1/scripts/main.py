8aad1268b49cfb766846aab5e7160585e2578a65
hectorfrs <67123958+hectorfrs@users.noreply.github.com>
2024-12-17 09:02:49 -0400

    update

--- src/pi1/scripts/main.py
+++ src/pi1/scripts/main.py
@@ -13,6 +13,7 @@ from utils.network_manager import NetworkManager
 from utils.real_time_config import RealTimeConfigManager
 from utils.config_manager import ConfigManager
 from utils.mqtt_publisher import start_publisher
+from utils.mqtt_client import create_mqtt_client, publish_message, subscribe_to_topic
 
 # Configuración de logging
 logging.basicConfig(level=logging.INFO, format="[%(asctime)s] [%(levelname)s] %(message)s", datefmt='%Y-%m-%d %H:%M:%S')
